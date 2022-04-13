66babdda2fd805272216a80bed9922f617516ea1
Ihsan Ahmad <nahidboss96@gmail.com>
2022-04-13 16:36:31 +0600

    Update

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction1() {
+    alert("Once you lost it, you lost it!");
+}
+function myFunction2() {
+    alert("Why you need an account?");
+}
+function myLogin() {
+    alert("When you enter the world of wisdom, there is no way back");
+}
